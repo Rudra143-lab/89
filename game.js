47308e8ee77b07f2b654bb3c42eb3afd9d336c53
Rudra143-lab <77666625+Rudra143-lab@users.noreply.github.com>
2021-05-09 13:12:40 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,9 @@
+
+function change_name(){
+
+Player_1=localStorage.getItem("player_1");
+
+Player_2=localStorage.getItem("player_2");
+
+document.getElementById("player1_name").innerHTML=Player_1 ;
+document.getElementById("player2_name").innerHTML=Player_2 ;}
